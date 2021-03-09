7b53302ed519d0c904593206f0007c63e8dee4ea
Lanuvelza <ianlam1221@hotmail.com>
2021-03-09 20:13:17 +0000

    testing feature for product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details page from home page", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # VERIFY 
+    first('article.product').find_link('Details').click
+
+    expect(page).to have_content('Name')  
+    expect(page).to have_content('Description')  
+    expect(page).to have_content('Quantity')
+    expect(page).to have_content('Price')
+    # DEBUG 
+    save_screenshot
+  end
+
+
+end
